630314a92d7a201f709a78926d2b6f00ecacec6a
rangaraj.s <rangaraj.sam@gmail.com>
2026-03-19 18:07:04 +0530

    host webpack and package files updated

---
+++ config/webpack.prod.js
@@ -0,0 +1,44 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const {ModuleFederationPlugin} = require('webpack').container;
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/',
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                products: `products@${domain}/products/latest/remoteEntry.js`,
+                header: `header@${domain}/header/latest/remoteEntry.js`,
+                authorization: `authorization@${domain}/authorization/latest/remoteEntry.js`,
+                cart: `cart@${domain}/cart/latest/remoteEntry.js`,
+                wishlist: `wishlist@${domain}/wishlist/latest/remoteEntry.js`,
+                profile: `profile@${domain}/profile/latest/remoteEntry.js`
+            },
+            shared: {
+                ...packageJson.dependencies,
+                react: {
+                    singleton: true,
+                    requiredVersion: packageJson.dependencies.react,
+                },
+                'react-dom': {
+                    singleton: true,
+                    requiredVersion: packageJson.dependencies['react-dom'],
+                },
+                'react-router-dom': {
+                    singleton: true,
+                    requiredVersion: packageJson.dependencies['react-router-dom'],
+                },
+            },
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
