105bcef74edc519eb6234ba0464eef0aab26a05b
rangaraj.s <rangaraj.sam@gmail.com>
2026-03-22 12:15:31 +0530

    domain removed

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,7 +3,6 @@ const commonConfig = require('./webpack.common');
 const {ModuleFederationPlugin} = require('webpack').container;
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: 'production',
@@ -15,12 +14,12 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                products: `products@${domain}/products/latest/remoteEntry.js`,
-                header: `header@${domain}/header/latest/remoteEntry.js`,
-                authorization: `authorization@${domain}/authorization/latest/remoteEntry.js`,
-                cart: `cart@${domain}/cart/latest/remoteEntry.js`,
-                wishlist: `wishlist@${domain}/wishlist/latest/remoteEntry.js`,
-                profile: `profile@${domain}/profile/latest/remoteEntry.js`
+                products: `products@/products/latest/remoteEntry.js`,
+                header: `header@/header/latest/remoteEntry.js`,
+                authorization: `authorization@/authorization/latest/remoteEntry.js`,
+                cart: `cart@/cart/latest/remoteEntry.js`,
+                wishlist: `wishlist@/wishlist/latest/remoteEntry.js`,
+                profile: `profile@/profile/latest/remoteEntry.js`
             },
             shared: {
                 ...packageJson.dependencies,
